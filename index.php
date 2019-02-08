02f1b31e82984248bd6868111286fd6e19ce2ec6
mohan548 <36624349+mohan548@users.noreply.github.com>
2019-02-08 12:01:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // This results in an error.
 // The output above is before the header() call
-header('Location: http://169.254.169.254/latest/meta-data/');
+header('Location: https://692abc92.ngrok.io');
 ?> 
